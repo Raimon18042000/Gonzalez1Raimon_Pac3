864574f2761303a33e09f0008591343b08fba411
Raimon Gonzalez Ribera <raimongonzalezribera@gmail.com>
2021-01-12 12:48:03 +0100

    Correcions i pujada de arxius

---
+++ calcula/calcula.c
@@ -0,0 +1,11 @@
+int main()
+{
+	int a=10;
+	int b=5;
+	
+	printf("La suma de %d i %d es %d\n",a,b, suma(a,b));
+	printf("El valor major entre %d i %d es %d\n",a,b, major(a,b));
+	printf("La resta entre %d i %d es %d\n",a,b, resta(a,b));
+	printf("La multiplicació de d% i d% és %d\n",a,b, multiplica(a,b));
+	printf("La divisió entre %d i %d és %d\n",a,b,divideix(a,b));
+}
